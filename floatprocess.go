a96acfd5b5fe1af5aa5e8e1b1ff3ada49ab9b8a0
kingram <kingram@163.com>
2021-08-21 16:03:37 +0800

    first commit

---
+++ floatprocess.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"strconv"
+)
+
+func ProcessFloat(f float64) float64 {
+	num2, _ := strconv.ParseFloat(fmt.Sprintf("%.8f", f), 64)
+	return num2
+}
+
+//big.NewRat(1, 1).SetFloat64(coinPrice.GRes.Data.Quote.USD.TotalVolume24H).FloatString(0)
+func ConvertFloatToString(f float64) string{
+	return big.NewRat(1, 1).SetFloat64(f).FloatString(0)
+}
---
+++ httpUtil/httpUtil.go
@@ -0,0 +1,59 @@
+package httpUtil
+
+import (
+	"context"
+	"fmt"
+	"github.com/pkg/errors"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"time"
+)
+
+func HttpGet(url string, params map[string]string, headers map[string]string) ([]byte, error) {
+	c := http.Client{
+		Transport: &http.Transport{
+			DialContext: (&net.Dialer{
+				Timeout: time.Second,
+			}).DialContext,
+		},
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 2000*time.Millisecond)
+	defer cancel()
+	//new request
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return []byte{}, errors.New("new request is fail ")
+	}
+	req.Header.Set("Accepts", "application/json")
+	req.Header.Add("X-CMC_PRO_API_KEY", "27f65df8-ff71-49a9-829f-176b3a41426b")
+	//add params
+	q := req.URL.Query()
+	if params != nil {
+		for key, val := range params {
+			q.Add(key, val)
+		}
+		req.URL.RawQuery = q.Encode()
+	}
+	//add headers
+	if headers != nil {
+		for key, val := range headers {
+			req.Header.Add(key, val)
+		}
+	}
+	//http client
+	fmt.Printf("Go %s URL : %s \n", http.MethodGet, req.URL.String())
+	do, err := c.Do(req.WithContext(ctx))
+	if err != nil {
+		return []byte{}, errors.Wrap(err, "do req failed.")
+	}
+	defer do.Body.Close()
+
+	bytesBody, err := ioutil.ReadAll(do.Body)
+	if err != nil {
+		return []byte{}, errors.New("read body failed")
+	}
+
+	fmt.Printf("HttpGet receive : %s \n", string(bytesBody))
+	return bytesBody, nil
+}
