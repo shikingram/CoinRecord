fc6fc7e3c8997bb94ad65bba76f397754feea50c
kingram <kingram@163.com>
2021-11-02 10:18:17 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+export GOPROXY=https://goproxy.io,direct
+go build CoinRecord
+chmod +x CoinRecord
 export https_proxy=http://127.0.0.1:1087/
+export http_proxy=http://127.0.0.1:1087/
 ./CoinRecord
